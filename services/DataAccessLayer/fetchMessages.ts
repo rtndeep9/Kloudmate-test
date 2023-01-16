8998490d9fa1a3b065c582e8700e2eaa0ab3752b
Ratandeep Arunkumar <ratandeeparun@gmail.com>
2023-01-16 20:32:30 +0530

    fetchMessages from db done

---
+++ services/DataAccessLayer/fetchMessages.ts
@@ -0,0 +1,43 @@
+import { Table } from "@serverless-stack/node/table";
+import { DynamoDB } from "aws-sdk";
+
+const ddb = new DynamoDB();
+
+export const fetchMessagesDal = async (desc:boolean, startDate:any, endDate:any) => {
+    try {
+        const result: any[] = [];
+        const params : DynamoDB.ScanInput = {
+            ExpressionAttributeValues: {
+                ':s': {S: startDate},
+                ':e' : {S: endDate},
+            },
+            FilterExpression: 'createdAt BETWEEN :s and :e',
+            ProjectionExpression: 'message,createdAt',
+            TableName: Table.FailedMessages4.tableName,            
+        }
+
+        const messages: DynamoDB.ScanOutput = await ddb.scan(params).promise();
+        
+        if(messages.Items) {
+            for (let message of messages.Items) {
+                result.push(DynamoDB.Converter.unmarshall(message))
+            }
+        }
+
+        //Sort Messages
+        if(!desc) {
+            result.sort((a,b) => {
+                return a.createdAt > b.createdAt ? 1 : a.createdAt === b.createdAt ? 0 : -1
+            })
+        } else {
+            result.sort((a,b) => {
+                return a.createdAt > b.createdAt ? - 1 : a.createdAt === b.createdAt ? 0 : 1
+            })
+        }
+
+        return { status: 200, result: result };
+    } catch (err) {
+        const error = err as Error;
+        return { status: 400, errorMessage: error.message };
+    }
+};
